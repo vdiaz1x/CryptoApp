5799d4c6e3ef2b515cd32a47e4d2ca2b8c3f4c07
Vinicio Diaz <vdiaz1x@gmail.com>
2018-05-08 17:50:51 -0400

    fixed schema, adding seed

---
+++ db/seed.js
@@ -0,0 +1,39 @@
+// code to run fake dummy data
+// borrowed from class work
+
+const util = require('util');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
+const db = require('../config/connect');
+const User   = require('../user/userDB')(db);
+
+const readFile = util.promisify(fs.readFile);
+
+readFile('./db/users.csv', 'utf8')
+  .then((data) => {
+    const lines = data.trim().split('\n');
+    // pop off the header row and the empty bottom row
+    lines.shift();
+    // loop over and insert the users into the db
+    return Promise.all(lines.map(async (record) => {
+      // destructure the record once we've split the row
+      const [firstname, lastname, username, email, zip, avatar, password] = record.split(',');
+
+      // hash a password
+      const pwdHash  = await bcrypt.hash(password, 11);
+
+      return User.save({
+        username,
+        email,
+        firstname,
+        lastname,
+        avatar,
+        zip,
+        password_digest: pwdHash,
+      });
+    }));
+  })
+  .then(data => console.log({ data }))
+  .catch(err => console.error({ err }));
+
