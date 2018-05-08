efc87a3f234b371825f99031120db2867f2a03a2
Aasim Sayani <aasimsayani@Aasims-MacBook-Air.local>
2018-05-07 23:23:50 -0400

    Updated Routes and Response Handlers

--- comment/commentResponse.js
+++ comment/commentResponse.js
@@ -1,1 +1,20 @@
-// this is the file for the responses (views) for the comment routes
+module.exports = {
+  sendJson(req, res) {
+    res.json(res.locals.tasks || res.locals.task);
+  },
+  // handCreate(req, res) {
+  //   res.set({
+  //     location: `${req.originalUrl}/${res.locals.user.user_id}`,
+  //   });
+  //   res.status(201).end();
+  // },
+
+  deleteHandle(req, res) {
+    res.status(204).end();
+  },
+
+  send404(err, req, res, next) {
+    console.log(error);
+    res.status(404).send('Please come back this page is under development');
+  },
+};
--- user/userRouter.js
+++ user/userRouter.js
@@ -1,1 +1,18 @@
-// this is the file for the user route
+// this is the file for the comment route
+/**
+*@module user/userRouter
+*@param {module} userRouter
+*@param {module} userController
+*@param {module} responseHandler
+*@return
+*/
+
+module.exports = (userRouter, userController, responseHandler) => {
+
+  userRouter.route('/:id')
+    .get(userController.getOneUser, responseHandler.sendJson)
+    .post(userController.updateUser, responseHandler.sendJson)
+
+  userRouter.route('/')
+    .get(userController.addUser, responseHandler.addJson);
+}
